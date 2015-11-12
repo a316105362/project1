35ad526c81ff85d1b9f78b0a49fc07b6bbed8515
zyl <316105362@qq.com>
2015-11-12 18:57:49 +0800

    first

---
+++ MSP430F55xx_1.c
@@ -0,0 +1,50 @@
+//******************************************************************************
+//   MSP430F552x Demo - Software Toggle P1.0
+//
+//   Description: Toggle P1.0 by xor'ing P1.0 inside of a software loop.
+//   ACLK = 32.768kHz, MCLK = SMCLK = default DCO~1MHz
+//
+//                MSP430F552x
+//             -----------------
+//         /|\|                 |
+//          | |                 |
+//          --|RST              |
+//            |                 |
+//            |             P1.0|-->LED
+//
+//   Bhargavi Nisarga
+//   Texas Instruments Inc.
+//   April 2009
+//   Built with CCSv4 and IAR Embedded Workbench Version: 4.21
+//******************************************************************************
+#include <msp430f5529.h>
+
+void main(void)
+{
+  volatile unsigned int i;
+
+  WDTCTL = WDTPW+WDTHOLD;                   // Stop WDT
+  P1DIR |= BIT0;                            // P1.0 set as output
+  P8DIR |= BIT1;
+  P8DIR |= BIT2;
+  P1DIR = BIT0;
+  P8DIR = BIT1;
+  P8DIR = BIT2;
+  
+  while(1)                                  // continuous loop
+  {
+    P1OUT ^= BIT0;                          // XOR P1.0
+    for(i=50000;i>0;i--);  
+    P1OUT ^= BIT0;
+    
+    P8OUT ^= BIT1;
+    
+    for(i=50000;i>0;i--);
+    P8OUT ^= BIT1;
+   
+    P8OUT ^= BIT2;
+    for(i=50000;i>0;i--);
+    P8OUT ^= BIT2;
+  
+  }
+}
